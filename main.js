b90eaa5ca19a8b853ee87823d62a4fd6a53ddc40
Mela <88276514+NotMelY@users.noreply.github.com>
2022-04-05 20:58:12 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+console.log('gello world');
+console.error("this is an error my g")
+console.warn('this is a warning')
+
+// var (dont use it anymore), const, let
+// const cant change value while let u can reasign new values.
+
+// DATA TYPES - string, numbers, boolean, null, undefined.
+
+let year = 2006
+
+// string
+const name = 'Hyari';
+//number
+let age = 15;
+// decimal
+const rating = 4.5;
+//boolean
+const isCool = true;
+//object? an error? its null too?
+const x = null
+//undefined
+const y = undefined;
+//undefined
+let z; 
+
+console.log(typeof name)
+
+//concatenation
+console.log("my name is " + name + " and i am " + age);
+//template string
+console.log(`My name is ${name} and i am ${age}`);
